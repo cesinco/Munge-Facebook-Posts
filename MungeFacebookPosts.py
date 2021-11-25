c5075908f37e90186d575f30eaac055aad8d6e35
cesinco <cesinco@hotmail.com>
2021-11-24 23:51:05 -0500

    Better algorithm for splitting and joining words

--- MungeFacebookPosts.py
+++ MungeFacebookPosts.py
@@ -8,11 +8,27 @@ def randomSwapLetters(word, swapCount=1):
     # We want to draw 2 distinct positions for each swap
     # This explains why we multiply the swapCount by 2
     # and also why we don't want to do a select with replacement
-    idx = sorted(
-        list(rng.choice(
-            np.array(list(range(len(word)))), size=2*swapCount, replace=False
-        ))
-    )
+    # We should also try to avoid swapping two letters that are identical for each other
+    # For example in the word "science" that characters in positions 1 and 5 are the same "c"
+    # At the same time, there may be words the no matter how much we try to swap, will never succeed
+    # For example the word "need" will never be anything other than "need"
+    # (since we preserve first and last characters in their respective positions)
+    loopMax = 10
+    i = 0
+    while True:
+        i += 1
+        idx = sorted(
+            list(rng.choice(
+                np.array(list(range(len(word)))), size=2*swapCount, replace=False
+            ))
+        )
+        if i >= loopMax:
+            break
+        else:
+            if word[idx[0]] != word[idx[1]]:
+                if swapCount > 1:
+                    if word[idx[2]] != word[idx[3]]:
+                        break
     # Swap at least 1 set of characters
     munged = word[:idx[0]] + word[idx[1]] + \
         word[idx[0] + 1: idx[1]] + word[idx[0]] + word[idx[1]+1:]
@@ -30,44 +46,34 @@ def main():
     # Collect the text for munging
     txtInput = input()
 
-    # Split entered text on space characters
-    splitWords = txtInput.split()
+    # Get a list of "delimiter" characters (basically anything non-alpha) from the input
+    splitReChars = list(re.sub(r'[A-Za-z]', '', txtInput))
 
-    # Using regular expression, grab any leading (pre) or trailing (post)
-    # text that is non-alpha from each word, using list comprehension
-    prepostfix = [
-        (re.search(r'(^[^A-Za-z]*)', x)[0], re.search(r'([^A-Za-z]*)$', x)[0])
-        for x in splitWords
-    ]
+    # Convert this list into a unique set of characters, then join them to create a regex pattern
+    # and use this pattern to extract only the words from the input
+    splitReWords = re.split(
+        '[' + ''.join(list(set(splitReChars))) + ']', txtInput)
 
-    # Using regular expression, grab any text between the leading (pre) or trailing (post)
-    # text that is non-alpha from each word, using list comprehension
-    words = [
-        re.sub(r'(^[^A-Za-z]*)', '', re.sub(r'([^A-Za-z]*)$', '', x))
-        for x in splitWords
-    ]
-
-    # Using only the alpha-ish text (note - we may still have non-alpha in the middle of a word)
+    # Using the words (composed of alpha-only) in the input text
     # perform the munging operation by calling the function we defined above on each
-    # word in the list, again using list comprehension to assemble the list
+    # word in the list, using list comprehension to assemble the list
     # The function randomSwapLetters is called with different parameters
     # depending on the length of the word being processed within the list.
     munged = [
         w if len(w) < 4
         else w[0] + randomSwapLetters(w[1:-1], 1) + w[-1] if len(w) < 12
         else w[0] + randomSwapLetters(w[1:-1], 2) + w[-1]
-        for w in words
+        for w in splitReWords
     ]
 
     # After munging each word, assemble the list back into a line of text
-    # which will also include adding any leading or trailing characters
-    # that were in the original "word".
-    mungedText = ' '.join([
-        prepostfix[i][0] + munged[i] + prepostfix[i][1]
-        if munged[i] != ''
-        else prepostfix[i][0]
-        for i in range(len(munged))
-    ])
+    # by interspacing the separators between each word. We do this by joining
+    # pairs of word + delimiter and then adding the final word at the end
+    # There will always be precisely one more word than there are delimiters.
+    mungedText = ''.join([
+        munged[i] + splitReChars[i]
+        for i in range(len(splitReChars))
+    ]) + munged[-1]
 
     # Print the result back to the user
     print(mungedText)
